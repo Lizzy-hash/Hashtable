d72a2da942dcfc90ee42d7bd5b668413d2811870
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-03-27 11:06:07 +0100

    some code organisation, nothing big, getting ready to send it to eliza;

--- zadanie1.cpp
+++ zadanie1.cpp
@@ -1,153 +1,147 @@
 ﻿#include <iostream>
+#include <exception>
 
+namespace ek {
 
-struct para {
+    struct pair {
 
-    para() {
-        this->klucz = NULL;
-        this->value = nullptr;
-    }
-    para(unsigned klucz, char* tekst) {
-        this->klucz = klucz;
-        this->value = tekst;
-    }
+        pair() {
+            this->key = NULL;
+            this->value = nullptr;
 
-    long klucz;
-    char* value;
+        }
 
-    bool is_empty = true;
-};
+        pair(unsigned klucz, char *tekst) {
+            this->key = klucz;
+            this->value = tekst;
+        }
 
+        long key;
+        char *value;
 
-//void add(std::pair<int, char*> *tab,int size);
-//void print(std::pair<int, char*>* tab, int size);
-//void del(std::pair<int, char*>* tab, int size);
-//
-//bool parser(char* pol, std::pair<int, char*> *tab,int size);
+        bool is_empty = true;
+    };
+}
 
-void add(para * tab, int size);
-void print(para* tab, int size);
-void del(para* tab, int size);
+void add(ek::pair *tab, int tab_size);
 
-bool parser(char* pol, para* tab, int size);
+void print(ek::pair *tab, int size);
 
-int main()
-{
-    int l_przypadkow=0;
+void del(ek::pair *tab, int size);
+
+bool parser(char *polecenie, ek::pair *tab, int size);
+
+int main() {
+    int l_przypadkow = 0;
     int size = 0;
     bool kon;
     char polecenie[20];
-    //std::pair<int, char*> *tab; 
 
-    para* tab;
+    ek::pair *tab;
 
     std::cin >> l_przypadkow;
 
-    for (int i = 0; i < l_przypadkow; i++)
-        {
+    for (int i = 0; i < l_przypadkow; i++) {
         std::cin >> polecenie;
         std::cin >> size;
 
-       // tab = new std::pair<int, char*>[size];
+        tab = new ek::pair[size];
+        do {
+            std::cin >> polecenie;
 
-        tab = new para[size];
-
-      //  memset(tab, true, size * sizeof(std::pair<int, char*>)); //trzeba zmienić, bo klucz może równać się 0;
-
-            do
-                {
-                std::cin >> polecenie;
-
-                kon = parser(polecenie, tab,size);
+            kon = parser(polecenie, tab, size);
 
 
-            } while (kon == true);
+        } while (kon);
 
-            delete[] tab;
-        }
+        delete[] tab;
+    }
 
-    
-   
+
     return 0;
 }
 
-void add(para* tab, int size) 
-{
-    long klucz;
-    char wartosc[9];
+void add(ek::pair *tab, int tab_size, ek::pair data) {
+    int index = data.key % tab_size;
 
-    std::cin >> klucz;
-    std::cin >> wartosc;
+    if (tab[index].is_empty || tab[index].key == data.key) {
+        tab[index] = data;
 
-    int index = klucz % size;
+    } else {
 
-    if (tab[index].is_empty || tab[index].klucz == klucz) {
-             tab[index] = para(klucz, wartosc);
-             tab[index].is_empty = false;
-        }
-    else
-        {
-            while(!tab[index].klucz)
-            { 
-                if (index > size - 1) index = 0;
-                else index++;
-            }
-            
-            tab[index] = para(klucz, wartosc);
-            tab[index].is_empty = false;
-        }
-    
+        do {
+            if (index >= tab_size) throw std::bad_alloc();
+            else index++;
+        } while (!tab[index].is_empty);
+
+        tab[index] = data;
+
+    }
+
 
 }
 
-void print(para* tab, int size)
-{
+void print(ek::pair *tab, int size) {
+    for (int i = 0; i < size; i++) {
+        if (!tab[i].is_empty) {
+            std::cout << i           << " "
+                      << tab[i].key  << " "
+                      << tab[i].value<<std::endl; //todo zamiast imienia wywala smieci z pamieci << std::endl;
+        }
 
-  
 
-    for (int i = 0; i < size; i++)
-    {
-        if (!tab[i].is_empty)
-            {
-            std::cout << i << " " << tab[i].klucz << " " << tab[i].value; //zamiast imienia wywala smieci z pamieci TO DO 
-            std::cout << std::endl;
-            }
-        
-       
     }
     std::cout << std::endl;
 }
 
-void del(para* tab, int size) //pozniej bo nie umiem
+void del(ek::pair *tab, int size, int klucz) //pozniej bo nie umiem
 {
-    int klucz;
-    std::cin >> klucz;
+
 
     int index = klucz % size;
 
-    while (tab[index].is_empty|| tab[index].klucz != klucz)
-        {
-            if (index > size - 1) index = 0;
-            else index++;// zrobic zapetlenie
-        }
+    while (tab[index].is_empty || tab[index].key != klucz) {
+        if (index >= size) return;  // nothing to delete
+        ++index;
+    }
 
-
-    tab[index] = tab[index + 1]; //przesunac trzeba wszystko o jeden w lewo, tam gdzie indexy się nie zgadzaja z wyliczonym
-                                 //stop jak znajdziemy zero lub wrocimy do tego samego miejsca
-    //TO DO co jeżeli index==size?
-
-    //alternatywnie znajdujemy ostatni element, ktory pasuje i przesuwamy go na powstale puste miejsce <- ma mniej przesuniec 
-    //rekurencja, jak dziurka to porzadkujemy nizej
-    //sprawdzamy kolejne pozycje od delete czy pasuja na pusty index, jak tak to oznaczamy i sprawdzamy, az napotkamy dziurke
-    //uzyc albo algorytmu przesypujacego albo przenoszacego ostatni element
+    while(!tab[index].is_empty) {
+        tab[index] = tab[index + 1];
+        if (index == size-1) tab[size -1] = ek::pair();
+        ++index;
+    }
 }
 
-bool parser(char* pol, para* tab, int size)
-{
-    if (pol[0] == 'a') add(tab, size);
-    if (pol[0] == 'p') print(tab,size);
-    if (pol[0] == 'd') del(tab,size);
-    if (pol[0] == 's' && pol[1] == 't') return false;
-    
+bool parser(char *polecenie, ek::pair *tab, int size) {
+    switch (polecenie[0]) {
+        case 'a': {
+            ek::pair to_add;
+            std::cin >> to_add.key;
+            std::cin >> to_add.value;
+            to_add.is_empty = false;
+            add(tab, size, to_add);
+            break;
+        }
+        case 'p': {
+            print(tab, size);
+            break;
+
+        }
+        case 'd': {
+            int key;
+            std::cin>>key;
+            del(tab, size,key);
+            break;
+        }
+        case 's': {
+
+            return false;
+        }
+        default :
+            // throw bad polecenie or something
+            break;
+
+    }
+
     return true;
 }
